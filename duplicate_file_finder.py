5dba8271490bf29bd6cb6f0123dc8dabfa1484e6
Hitesh Garg <hitesh.garg.34.hg@gmail.com>
2024-10-15 20:52:33 +0530

    refactored some code to make better use of functions
    refactored some code to make better use of functions

--- duplicate_file_finder.py
+++ duplicate_file_finder.py
@@ -5,47 +5,44 @@ import hashlib
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import argparse
 
-def list_files(directory: str, directory_to_skip: str) -> None:
+
+def no_duplicates():
+    print('Congrats, No Duplicates Found.')
+    sys.exit(0)
+
+def list_files(directory: str, directory_to_skip: str) -> list:
+    list_of_files = []
     # Traverse the directory and list all files
     for root, _, files in os.walk(directory):
         if directory_to_skip not in root:
             sp = (root.split(directory)[-1]).split('/')[-1]
             for file in files:
                 if bool(sp):
-                    print(sp+'/'+file)
+                    list_of_files.append(sp+'/'+file)
                 else:
-                    print(file)
+                    list_of_files.append(file)
+    return list_of_files
 
-def find_potential_duplicates_using_size(directory: str, directory_to_skip: str) -> None:
+def find_potential_duplicates_using_size(list_of_files: list) -> dict:
+    potential_duplicates_by_size={}
     # Traverse the directory and list all files
     file_size_to_name_mapper = defaultdict(list)
-    for root, _, files in os.walk(directory):
-        if directory_to_skip not in root:
-            sp = (root.split(directory)[-1]).split('/')[-1]
-            # print('sp', sp)
-            for file in files: 
+    for file_path in list_of_files:
+        # Get the file size
+        try:
+            file_size = os.path.getsize(file_path)
+            # Add file path to the list for that size
+            if file_size in file_size_to_name_mapper:
+                file_size_to_name_mapper[file_size].append(file_path)
+                if len(file_size_to_name_mapper[file_size])>1:
+                    # print('Potential duplicates:', *file_size_to_name_mapper[file_size])
+                    potential_duplicates_by_size[file_size] = file_size_to_name_mapper[file_size]
 
-                # Build the full file path
-                file_path = os.path.join(root, file)
-
-                # Remove the directory prefix for output
-                if bool(sp):
-                    relative_path = f"{sp}/{file}"
-                else:
-                    relative_path = file
-
-                # Get the file size
-                try:
-                    file_size = os.path.getsize(file_path)
-                    # Add file path to the list for that size
-                    if file_size in file_size_to_name_mapper:
-                        file_size_to_name_mapper[file_size].append(relative_path)
-                        if len(file_size_to_name_mapper[file_size])>1:
-                            print('Potential duplicates:', *file_size_to_name_mapper[file_size])
-                    else:
-                        file_size_to_name_mapper[file_size].append(relative_path)
-                except OSError as e:
-                    print(f"Error accessing file {file_path}: {e}")
+            else:
+                file_size_to_name_mapper[file_size].append(file_path)
+        except OSError as e:
+            print(f"Error accessing file {file_path}: {e}")
+    return potential_duplicates_by_size
 
 def compute_md5(file_path: str) -> str:
     """Compute MD5 checksum for a given file."""
@@ -60,46 +57,31 @@ def compute_md5(file_path: str) -> str:
         return None
     return hash_md5.hexdigest()
 
-def find_potential_duplicates_using_MD5_checksum(directory: str, directory_to_skip: str, min_size: int) -> dict:
-    potential_duplicates={}
-    # Traverse the directory and list all files
+def find_potential_duplicates_using_MD5_checksum(potential_duplicates: dict, min_size: int) -> dict:
+    potential_duplicates_by_md5={}
     file_size_to_name_mapper = defaultdict(list)
-    for root, _, files in os.walk(directory):
-        if directory_to_skip not in root:
-            sp = (root.split(directory)[-1]).split('/')[-1]
-            # print('sp', sp)
-            for file in files: 
 
-                # Build the full file path
-                file_path = os.path.join(root, file)
+    for file_size in potential_duplicates:
+        for file_path in potential_duplicates[file_size]:
+            # Get the file hash
+            try:
+                if file_size < min_size:
+                    continue
 
-                # Remove the directory prefix for output
-                if bool(sp):
-                    relative_path = f"{sp}/{file}"
+                file_hash = compute_md5(file_path)
+
+                # Add file path to the list for that size
+                if file_hash in file_size_to_name_mapper:
+                    file_size_to_name_mapper[file_hash].append(file_path)
+                    if len(file_size_to_name_mapper[file_hash])>1:
+                        # print('Potential duplicates:', *file_size_to_name_mapper[file_hash])
+                        potential_duplicates_by_md5[file_hash] = file_size_to_name_mapper[file_hash]
                 else:
-                    relative_path = file
+                    file_size_to_name_mapper[file_hash].append(file_path)
+            except OSError as e:
+                print(f"Error accessing file {file_path}: {e}")
 
-                # Get the file hash
-                try:
-                    file_size = os.path.getsize(file_path)
-
-                    if file_size < min_size:
-                        continue
-
-                    file_hash = compute_md5(file_path)
-
-                    # Add file path to the list for that size
-                    if file_hash in file_size_to_name_mapper:
-                        file_size_to_name_mapper[file_hash].append(relative_path)
-                        if len(file_size_to_name_mapper[file_hash])>1:
-                            # print('Potential duplicates:', *file_size_to_name_mapper[file_hash])
-                            potential_duplicates[file_hash] = file_size_to_name_mapper[file_hash]
-                    else:
-                        file_size_to_name_mapper[file_hash].append(relative_path)
-                except OSError as e:
-                    print(f"Error accessing file {file_path}: {e}")
-
-    return potential_duplicates
+    return potential_duplicates_by_md5
 
 def compare_two_files(file1: str, file2: str) -> bool:
     """Compare two files byte by byte."""
@@ -133,6 +115,30 @@ def compare_files_per_byte(files: list) -> bool:
                 return True
     return False
 
+def find_potential_duplicates_by_comparing_byte_by_byte(potential_duplicates_by_md5: dict) -> None:
+    duplicate_flag = False
+    for _ , pot_dup_list in potential_duplicates_by_md5.items():
+        duplicates = compare_files_per_byte(pot_dup_list)
+        if duplicates:
+            duplicate_flag = True
+            duplicate_str = ' '.join(pot_dup_list)
+            print(f'Duplicates: {duplicate_str}')
+            print('Which file should be deleted?')
+            for idx, file in enumerate(pot_dup_list):
+                print(str(idx+1)+') '+file)
+            try:
+                user_input=int(input())
+                if user_input in range(1,len(pot_dup_list)+1):
+                    os.remove(pot_dup_list[user_input-1]) ######### Step 6
+                    print('File deleted')
+                else:
+                    print('Incorrect Input, hence keeping the files.')
+            except Exception:
+                print('Incorrect Input, hence keeping the files.')
+
+    if not duplicate_flag:
+        no_duplicates()
+
 if __name__ == "__main__":
 
     # Use argparse to parse command-line arguments
@@ -155,38 +161,28 @@ if __name__ == "__main__":
 
     ######### Step 1
     # Call the function to list files
-    # list_files(directory, DIRECTORY_TO_SKIP)
+    list_of_files = list_files(directory, DIRECTORY_TO_SKIP)
+
+    if not list_of_files:
+        print('There are no files in this directory.')
+        sys.exit(0)
 
     ######### Step 2
     #find potential duplicates using file size
-    # find_potential_duplicates_using_size(directory, DIRECTORY_TO_SKIP)
+    potential_duplicates_by_size = find_potential_duplicates_using_size(list_of_files)
+
+    if not potential_duplicates_by_size:
+        no_duplicates()
 
     ######### Step 3 and Step 5
     # find potential duplicates using file MD5 checksum
-    potential_duplicates = find_potential_duplicates_using_MD5_checksum(directory, DIRECTORY_TO_SKIP, min_size)
+    potential_duplicates_by_md5 = find_potential_duplicates_using_MD5_checksum(potential_duplicates_by_size, min_size)
+    
+    if not potential_duplicates_by_md5:
+        no_duplicates()
 
     ######### Step 4
     #find duplicates by comparing files byte by byte
-    duplicate_flag = False
-    for _ , pot_dup_list in potential_duplicates.items():
-        duplicates = compare_files_per_byte(pot_dup_list)
-        if duplicates:
-            duplicate_flag = True
-            duplicate_str = ' '.join(pot_dup_list)
-            print(f'Duplicates: {duplicate_str}')
-            print('Which file should be deleted?')
-            for idx, file in enumerate(pot_dup_list):
-                print(str(idx+1)+') '+file)
-            try:
-                user_input=int(input())
-                if user_input in range(1,len(pot_dup_list)+1):
-                    os.remove(pot_dup_list[user_input-1]) ######### Step 6
-                    print('File deleted')
-                else:
-                    print('Incorrect Input, hence keeping the files.')
-            except Exception:
-                print('Incorrect Input, hence keeping the files.')
-
-    if not duplicate_flag:
-        print('Congrats, No Duplicates Found.')
+    find_potential_duplicates_by_comparing_byte_by_byte(potential_duplicates_by_md5)
+    
 
